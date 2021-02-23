0051c9674177526063b22487175deaf6ea004cf1
FelixEIGato <tcin@outlook.es>
2021-02-23 11:25:29 -0500

    Ajax fetch

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -18,10 +18,39 @@
             $xhr.appendChild($fragment);
         }else{
             console.log("error");
+            let message = xhr.statusText || "Ocurrió u error";
+            $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
+
         }
     })
 
     xhr.open("GET","https://jsonplaceholder.typicode.com/users");
 
     xhr.send();
+})();
+
+(()=>{
+    const $fetch = document.getElementById("fetch"),
+     $fragment = document.createDocumentFragment();
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+     .then((res)=>{
+         return res.ok ? res.json() : Promise.reject(res);
+     })
+     .then((json)=>{
+         json.forEach(el=>{
+             const $li = document.createElement("li");
+             $li.innerHTML = `${el.name}--${el.email}--${el.phone}`;
+             $fragment.appendChild($li);
+         });
+
+         $fetch.appendChild($fragment);
+     })
+     .catch((err)=>{
+         let message = err.statusText || "Ocurrió un error";
+         $fetch.innerHTML = `Error ${err.status}: ${message}`;
+     })
+     .finally(()=>{
+         console.log("Se ejecuta independientemente");
+     })
 })();
